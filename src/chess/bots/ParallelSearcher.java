96872f8de9abce7038b3f73b5dc11166f4141427
jorycw <jorycw@brudley.cs.washington.edu>
2018-08-02 05:44:46 -0700

    working simple search

--- src/chess/bots/ParallelSearcher.java
+++ src/chess/bots/ParallelSearcher.java
@@ -1,14 +1,91 @@
 package chess.bots;
 
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
 import cse332.chess.interfaces.AbstractSearcher;
 import cse332.chess.interfaces.Board;
+import cse332.chess.interfaces.Evaluator;
 import cse332.chess.interfaces.Move;
 import cse332.exceptions.NotYetImplementedException;
 
 
 public class ParallelSearcher<M extends Move<M>, B extends Board<M, B>> extends
         AbstractSearcher<M, B> {
+	
+	private static final ForkJoinPool POOL = new ForkJoinPool();
+	protected static int divideCutoff = 5;
+	
     public M getBestMove(B board, int myTime, int opTime) {
-        throw new NotYetImplementedException();
+        return parallel(this.evaluator, board, ply).move;
+    }
+    
+    public <M extends Move<M>, B extends Board<M, B>> BestMove<M> parallel(Evaluator<B> evaluator, B board, int depth) {
+    	List<M> moves = board.generateMoves();
+    	return POOL.invoke(new SearchTask(evaluator, board, depth, 0, moves.size(), moves));
+    }
+    
+    private class SearchTask<M extends Move<M>, B extends Board<M, B>> extends RecursiveTask<BestMove<M>> {
+    	
+    	Evaluator<B> evaluator;
+    	B board;
+    	int depth, lo, hi;
+    	List<M> moves;
+    	// goes through list from lo(inclusive) to hi(exclusive)
+    	public SearchTask(Evaluator<B> evaluator, B board, int depth, int lo, int hi, List<M> moves) {
+    		this.evaluator = evaluator;
+    		this.board = board;
+    		this.depth = depth;
+    		this.lo = lo;
+    		this.hi = hi;
+    		this.moves = moves;
+    	}
+    	
+    	@Override
+    	protected BestMove<M> compute() {
+    		
+	    	if (moves.isEmpty()) {
+	    		if (board.inCheck() ) {
+	    			return new BestMove<M>(-evaluator.mate() - depth);
+	    		} else {
+	    			return new BestMove<M>(-evaluator.stalemate());
+	    		}
+	    	}
+    		
+    		if (depth <= cutoff) {
+    			return SimpleSearcher.minimax(evaluator, board, depth);
+    		}
+    		if (hi - lo <= divideCutoff) {
+    			BestMove<M> max = new BestMove<M>(-evaluator.infty());
+    			
+    			for (int i = lo; i < hi; i++) {
+    				board.applyMove(moves.get(i));
+    				
+    				BestMove<M> bMove = new SearchTask(evaluator, board, depth, lo, mid, moves);
+    						
+    				board.undoMove();
+    				
+    			}
+    			
+    			
+    			
+    		}
+    		int mid = lo + (hi - lo) / 2;
+    		SearchTask left = new SearchTask(evaluator, board, depth, lo, mid, moves);
+    		SearchTask right = new SearchTask(evaluator, board, depth, mid, hi, moves);
+    		
+    		left.fork();
+    		
+    		BestMove<M> rightResult = right.compute();
+    		BestMove<M> leftResult = (BestMove<M>) left.join();
+    		
+    		if (leftResult.value > rightResult.value) {
+    			return leftResult;
+    		} else {
+    			return rightResult;
+    		}
+    	}
+    	
     }
 }
--- src/chess/bots/SimpleSearcher.java
+++ src/chess/bots/SimpleSearcher.java
@@ -1,10 +1,11 @@
 package chess.bots;
 
+import java.util.List;
+
 import cse332.chess.interfaces.AbstractSearcher;
 import cse332.chess.interfaces.Board;
 import cse332.chess.interfaces.Evaluator;
 import cse332.chess.interfaces.Move;
-import cse332.exceptions.NotYetImplementedException;
 
 /**
  * This class should implement the minimax algorithm as described in the
@@ -18,8 +19,36 @@ public class SimpleSearcher<M extends Move<M>, B extends Board<M, B>> extends
         BestMove<M> best = minimax(this.evaluator, board, ply);
         return best.move;
     }
-
+    
     static <M extends Move<M>, B extends Board<M, B>> BestMove<M> minimax(Evaluator<B> evaluator, B board, int depth) {
-        throw new NotYetImplementedException();
+    	if (depth == 0) {
+    		return new BestMove<M>(evaluator.eval(board));
+    	} else {
+    	
+	    	List<M> moves = board.generateMoves();
+	    	BestMove<M> max = new BestMove<M>(-evaluator.infty());
+	    	
+	    	if (moves.isEmpty()) {
+	    		if (board.inCheck() ) {
+	    			return new BestMove<M>(-evaluator.mate() - depth);
+	    		} else {
+	    			return new BestMove<M>(-evaluator.stalemate());
+	    		}
+	    	}
+	    	
+	    	for (M move: moves) {
+	    		board.applyMove(move);
+	    		BestMove<M> bMove = minimax(evaluator, board, depth - 1).negate();
+	    		
+	    		if (bMove.value > max.value) {
+	    			max.value = bMove.value;
+	    			max.move = move;
+	    		}
+	    		
+	    		board.undoMove();
+	    	}
+	    	return max;
+    	}
     }
 }
+    
